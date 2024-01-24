59a9f2e52a8dd46975f2693e7bbb894727d6c42c
leagsawed <gael.dewas@gmail.com>
2024-01-24 14:32:49 +0100

    get data

---
+++ js/script.js
@@ -0,0 +1,17 @@
+fetch("http://localhost:3000/api/products")
+  .then((res) => res.json())
+  .then((data) => addProducts(data));
+
+function addProducts(donnees) {
+  const imageUrl = donnees[0].imageUrl;
+
+  const anchor = document.createElement("a");
+
+  anchor.href = imageUrl;
+  anchor.text = "un super canap";
+
+  const items = document.querySelector("#items");
+  if (items != null) {
+    items.appendChild(anchor);
+  }
+}
